234d14a96138b4b589155137745593627b335070
Josh Hall <jhall@sgcaptrading.com>
2018-02-05 12:26:22 -0600

    Finished Blog!

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,11 @@ const Header = (props) => (
       <h1 className="header__title">{props.title}</h1>  
        
       {!!!props.username ? 
-        <button className="button" onClick={props.handleOpenLogin}>Login</button> : 
+        <div className="container__min">
+          <h3 className="header__subtitle">Login if you want to post!</h3>
+          <button className="button" onClick={props.handleOpenLogin}>Login</button> 
+        </div>
+        : 
         <div className="container__min">
           {!!props.username && <h3 className="header__subtitle">{props.username}</h3>}   
           <button className="button" onClick={props.handleCreateButtonPress}>Create Post</button>
